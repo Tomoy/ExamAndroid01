9f32bedb2b705d376811b268cb54962ec23005de
Tomás Ignacio Moyano <moyanotomasi@gmail.com>
2017-11-12 19:33:48 +0100

    First Commit with project creation and basic package structure

---
+++ app/src/main/java/com/tomasm/android01exam/Constants.kt
@@ -0,0 +1,3 @@
+package com.tomasm.android01exam
+
+val DISHES_API_URL = "http://www.mocky.io/v2/5a088e2b3200007406137ff7"
---
+++ app/src/main/java/com/tomasm/android01exam/activity/TableActivity.kt
@@ -0,0 +1,13 @@
+package com.tomasm.android01exam.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tomasm.android01exam.R
+
+class TableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table)
+    }
+}
